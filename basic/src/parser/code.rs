36f8631ced8b833faecfe2aa8d17bb47fad77c6c
orasunis <orasunis@protonmail.com>
2018-03-18 13:04:37 +0100

    Fixed pushing too many Nones to the instructions of Code

--- basic/src/parser/code.rs
+++ basic/src/parser/code.rs
@@ -21,6 +21,7 @@ pub fn parse_code(decoder: &mut Decoder, constant_pool: &Pool) -> Result<Attribu
         loop {
             let (end, instruction) = parse_instruction(&mut code_decoder, code_location)?;
             instructions.push(Some(instruction));
+            code_location += 1;
             while code_location != end {
                 instructions.push(None);
                 code_location += 1;
